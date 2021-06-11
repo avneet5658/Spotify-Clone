dcc3fe5a68e52ae9a5ddd124c3e86506803d37d7
avneet5658 <avneet.pandey@mail.vinove.com>
2021-06-11 15:26:40 +0530

    refresh token

--- server/server.js
+++ server/server.js
@@ -1,13 +1,38 @@
 const express = require("express");
 const SpotifyWebApi = require("spotify-web-api-node");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
-app.post("/", (req, res) => {
+app.use(cors());
+app.use(express.json());
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "43bd539785024e6ea2db929033117b78",
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  });
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      console.log(data.body);
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      console.log("Could not refresh access token", err);
+    });
+});
+app.post("/login", (req, res) => {
   const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
     redirectUri: "http://localhost:3000",
     clientId: "43bd539785024e6ea2db929033117b78",
-    clientSecret: "fb8d216281244b35bddfbb354e61bad3",
+    clientSecret: process.env.CLIENT_SECRET,
   });
   spotifyApi
     .authorizationCodeGrant(code)
@@ -21,4 +46,8 @@ app.post("/", (req, res) => {
     .catch((err) => {
       res.status(400).json({ err });
     });
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`connection establishde at ${PORT}`);
 });
